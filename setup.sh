27601034ee141eb8b6156bc22edf30d7f8ef6ed5
jordan <jordan@taiv.tv>
2022-04-12 17:33:46 +0000

    brev setup

---
+++ setup.sh
@@ -0,0 +1,6 @@
+##### Node v14.x + npm #####
+(echo ""; echo "##### Node v14.x + npm #####"; echo "";)
+sudo apt install ca-certificates
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
